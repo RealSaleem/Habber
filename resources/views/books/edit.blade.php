ed50ac8b4783fc144426eeedac27a0f1036aa2d5
hamzafaqhi <hamzafaqhi@gmail.com>
2020-10-20 18:40:16 +0500

    merged localization branch with datatablechnage

--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -80,7 +80,8 @@
                         <label for="cono1" class="col-sm-3 text-right control-label col-form-label">@lang('messages.book_page.quantity')</label>
                         <div class="col-sm-9">
                             <input type="number" class="form-control"  min="0" name="quantity" id="quantity"value="{{$book->quantity}}" placeholder="Quantity">
-                            <span class="text-danger">{{$errors->first('quantity')}}</span>
+                            <span class="text-danger">{{$errors->first('quantity')}}
+                            </span>
                         </div>
                     </div>
                     <div class="form-group row">
