47310ac2f66aa921ed5510fdd8f1ab0dd0cb650f
hamzafaqhi <hamzafaqhi@gmail.com>
2020-11-06 17:54:47 +0500

    added more permission in seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
        
-        // $this->call(PermissionTableSeeder::class);
+        $this->call(PermissionTableSeeder::class);
         // $this->call(CountryTableSeeder::class);
         // $this->call(AdminSeeder::class);
         // $this->call(CurrencyTableSeeder::class);
-        $this->call(SiteSettingSeeder::class);
+        // $this->call(SiteSettingSeeder::class);
     }
 }
