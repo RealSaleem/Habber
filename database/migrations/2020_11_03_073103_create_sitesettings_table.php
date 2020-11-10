497f62cb780ca13d8871fc9e15632458f9b84588
hamzafaqhi <hamzafaqhi@gmail.com>
2020-11-10 15:37:38 +0500

    commented site setting migration

--- database/migrations/2020_11_03_073103_create_sitesettings_table.php
+++ database/migrations/2020_11_03_073103_create_sitesettings_table.php
@@ -11,30 +11,30 @@ class CreateSitesettingsTable extends Migration
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('site_settings', function (Blueprint $table) {
-            $table->id();
-            $table->string('email');
-            $table->integer('currency');
-            $table->string('language');
-            $table->integer('phone_no');
-            $table->integer('whatsaap_number');
-            $table->string('twitter_url');
-            $table->string('facebook_url');
-            $table->string('instagram_url');
-            $table->string('snapchat_url');
-            $table->timestamps();
-        });
-    }
+    // public function up()
+    // {
+    //     Schema::create('site_settings', function (Blueprint $table) {
+    //         $table->id();
+    //         $table->string('email');
+    //         $table->integer('currency');
+    //         $table->string('language');
+    //         $table->integer('phone_no');
+    //         $table->integer('whatsaap_number');
+    //         $table->string('twitter_url');
+    //         $table->string('facebook_url');
+    //         $table->string('instagram_url');
+    //         $table->string('snapchat_url');
+    //         $table->timestamps();
+    //     });
+    // }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('site_settings');
-    }
+    // /**
+    //  * Reverse the migrations.
+    //  *
+    //  * @return void
+    //  */
+    // public function down()
+    // {
+    //     Schema::dropIfExists('site_settings');
+    // }
 }
