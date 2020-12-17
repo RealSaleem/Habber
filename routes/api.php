23808dda52a1bc431e10ca95ad71bb154dac82db
Sameed1997 <syedsameedzafar97@gmail.com>
2020-12-17 19:12:45 +0500

    user profile api update

--- routes/api.php
+++ routes/api.php
@@ -69,7 +69,7 @@ Route::group(['namespace' => 'Api' , 'prefix' => 'v1'], function() {
         Route::delete('addresses/{id}', 'AddressController@destroy');
         Route::get('addresses/{id}', 'AddressController@show');
         // users
-        Route::put('user', 'UserController@update');
+        Route::post('user', 'UserController@update');
         Route::post('users/password', 'UserController@updatePassword');
             //static pages
             
