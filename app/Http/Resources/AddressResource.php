0639354ae37f2ba1816f31fcc71fdc19683a7045
tariq <mtaji5666@gmail.com>
2020-12-29 15:43:53 +0500

    update currency with saudi riyal & resolve crash book & bookmark

--- app/Http/Resources/AddressResource.php
+++ app/Http/Resources/AddressResource.php
@@ -20,13 +20,13 @@ class AddressResource extends JsonResource
             'address_name' => $this->address_name,
             'address_line1' => $this->address_line1,
             'address_line2' => $this->address_line2,
-            'country_id' => $this->countries->id,
+            'country_id' =>  optional($this->countries)->id,
             'city_id' => optional($this->cities)->id,
             'phone' => $this->phone,
             'state' => $this->state,
             'post_code' => $this->post_code,
             'user_id' => $this->user_id,
-            'shipping_charges'=>auth()->user()->currencies->symbol. number_format(optional($this->cities)->shipping_charges * auth()->user()->currencies->rate,4)
+            //'shipping_charges'=>auth()->user()->currencies->symbol. number_format(optional($this->cities)->shipping_charges * auth()->user()->currencies->rate,4)
         ];
     }
 
