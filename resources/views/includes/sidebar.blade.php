447fe5820e0d890a985c801f19002a01410a0d7f
tariq <mtaji5666@gmail.com>
2021-02-03 15:20:03 +0500

    role tab assign to navbar

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -78,6 +78,12 @@
                         <li class="sidebar-item" style="background-color: #786e0047;"><a href="{{ route('banners.create') }}" class="sidebar-link"><i class="mdi mdi-note-plus"></i><span class="hide-menu"> @lang('messages.sidebar.create') </span></a></li>
                     </ul>
                 </li>
+                <li class="sidebar-item" > <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-receipt"></i><span class="hide-menu"> @lang('messages.role_page.role') </span></a>
+                    <ul aria-expanded="false" class="collapse  first-level">
+                        <li class="sidebar-item" style="background-color: #786e0047;"><a href="{{ route('roles.index') }}" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu"> @lang('messages.sidebar.list') </span></a></li>
+                        <li class="sidebar-item" style="background-color: #786e0047;"><a href="{{ route('roles.create') }}" class="sidebar-link"><i class="mdi mdi-note-plus"></i><span class="hide-menu"> @lang('messages.sidebar.create') </span></a></li>
+                    </ul>
+                </li>
                 </li>
                 <li class="sidebar-item" > <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-receipt"></i><span class="hide-menu"> @lang('messages.ad_page.ad') </span></a>
                     <ul aria-expanded="false" class="collapse  first-level">
