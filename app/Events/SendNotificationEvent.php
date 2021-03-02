ac318150760f24958bae9010cf281fc19d961f30
muhammadaamir <pertuma@gmail.com>
2021-03-02 18:13:58 +0500

    removing queable

--- app/Events/SendNotificationEvent.php
+++ app/Events/SendNotificationEvent.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class SendNotificationEvent implements ShouldQueue
+class SendNotificationEvent
 {
     
     use Dispatchable, InteractsWithSockets, SerializesModels;
